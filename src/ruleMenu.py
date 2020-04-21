676a8fb41e1811a924b30a9be318e263db2f67e9
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2020-04-21 10:11:33 +0200

    Create ruleMenu.py

---
+++ src/ruleMenu.py
@@ -0,0 +1,28 @@
+import pygame
+from settings import *
+from pygame import locals as const
+from button import Button
+from text import Text
+from menu import Menu
+
+class RuleMenu(object):
+    """docstring for RuleMenu."""
+
+    def __init__(self, main, screen):
+        self.main = main
+        self.screen = screen
+        self.colorMode = False
+        self.vsPlayer = False
+        self.new()
+
+    def new(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def events(self, event):
+        pass
